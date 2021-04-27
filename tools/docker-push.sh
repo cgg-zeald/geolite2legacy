468cb255f5937be812133b0412332a7e79b87d92
cgg-zeald <christopher.gabijan@zeald.com>
2021-04-27 14:37:42 +1200

    fix push mode

--- tools/docker-push.sh
+++ tools/docker-push.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker tag  geolite2legacy docker.pkg.github.com/cgg-zeald/geolite2legacy
-docker push docker.pkg.github.com/cgg-zeald/geolite2legacy/geolite2legacy:latest
+docker push docker.pkg.github.com/cgg-zeald/geolite2legacy:latest
